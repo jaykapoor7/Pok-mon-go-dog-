134c76ea30c46e11ae73dae4603bece2504c10de
jaykapoor7 <jaykapoor700@gmail.com>
2026-09-17 05:40:40 -0400

    Simplify community home data load

--- src/app/app/page.tsx
+++ src/app/app/page.tsx
@@ -1,19 +1,16 @@
 import { AppShell } from "@/components/app/AppShell";
 import { CommunityHome } from "@/components/app/CommunityHome";
 import { getAllDogs, getRecentSightings } from "@/lib/data";
-import { getPublicProgrammes } from "@/lib/public-programmes";
-import { ORGS } from "@/lib/platform/orgs";
 
 export const dynamic = "force-dynamic";
 export const metadata = { title: "Your neighbourhood, StrayPaw" };
 
 export default async function ConsoleHome() {
-  const [sightings, dogs, programmes] = await Promise.all([
+  const [sightings, dogs] = await Promise.all([
     getRecentSightings(100),
     getAllDogs(),
-    getPublicProgrammes(100),
   ]);
   return <AppShell>
-    <CommunityHome dogs={dogs} sightings={sightings} programmes={programmes} organisationCount={ORGS.length} />
+    <CommunityHome dogs={dogs} sightings={sightings} />
   </AppShell>;
 }
