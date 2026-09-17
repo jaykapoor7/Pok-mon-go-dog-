87a7cb04898e7a5c20690d610ca9ef468ed2c7d8
jaykapoor7 <jaykapoor700@gmail.com>
2026-09-17 00:49:57 -0400

    Add configurable project register workspace

---
+++ src/app/partner/projects/page.tsx
@@ -0,0 +1,8 @@
+import { ProjectRegisters } from "@/components/partner/ProjectRegisters";
+
+export const dynamic = "force-dynamic";
+export const metadata = { title: "Projects, StrayPaw Partner" };
+
+export default function PartnerProjectsPage(){
+  return <ProjectRegisters />;
+}
