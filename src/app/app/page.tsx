6e7c2982aa544770854450f8c17a4a904471bb54
jaykapoor7 <jaykapoor700@gmail.com>
2026-09-17 09:51:16 -0400

    Load recent rescue stories on community home

--- src/app/app/page.tsx
+++ src/app/app/page.tsx
@@ -1,16 +1,18 @@
 import { AppShell } from "@/components/app/AppShell";
 import { CommunityHome } from "@/components/app/CommunityHome";
 import { getAllDogs, getRecentSightings } from "@/lib/data";
+import { getPublicCaseStories } from "@/lib/community-case-stories";
 
 export const dynamic = "force-dynamic";
 export const metadata = { title: "Your neighbourhood, StrayPaw" };
 
 export default async function ConsoleHome() {
-  const [sightings, dogs] = await Promise.all([
+  const [sightings, dogs, stories] = await Promise.all([
     getRecentSightings(100),
     getAllDogs(),
+    getPublicCaseStories(),
   ]);
   return <AppShell>
-    <CommunityHome dogs={dogs} sightings={sightings} />
+    <CommunityHome dogs={dogs} sightings={sightings} stories={stories} />
   </AppShell>;
 }
