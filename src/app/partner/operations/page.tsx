19c18ecdc8c742eb56b60e8fecfb04cec3041e3f
jaykapoor7 <jaykapoor700@gmail.com>
2026-09-17 00:47:12 -0400

    Add NGO operations workspace

---
+++ src/app/partner/operations/page.tsx
@@ -0,0 +1,8 @@
+import { OperationsRegister } from "@/components/partner/OperationsRegister";
+
+export const dynamic = "force-dynamic";
+export const metadata = { title: "Operations, StrayPaw Partner" };
+
+export default function PartnerOperationsPage(){
+  return <OperationsRegister />;
+}
